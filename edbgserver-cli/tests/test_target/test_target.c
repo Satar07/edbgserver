6d9a6a198462b9eab0806f0466a72f68c70f973d
Cyril <multya77@gmail.com>
2026-02-21 22:53:56 +0800

    chore: Convert test target to use multiple threads

--- edbgserver-cli/tests/test_target/test_target.c
+++ edbgserver-cli/tests/test_target/test_target.c
@@ -1,21 +1,19 @@
-// test_target.c
 #include <stdio.h>
+#include <pthread.h>
 #include <unistd.h>
 
-__attribute__((noinline)) void trigger_breakpoint() {
-  printf("[Target] Trigger function called!\n");
-  int a = 1;
-  a = 2;
-  a = 3;
-}
+void logic_A() { printf("Thread A running\n"); sleep(1); }
+void logic_B() { printf("Thread B running\n"); sleep(1); }
+void logic_C() { printf("Thread C running\n"); sleep(1); }
+
+void* worker_A(void* arg) { while(1) logic_A(); }
+void* worker_B(void* arg) { while(1) logic_B(); }
+void* worker_C(void* arg) { while(1) logic_C(); }
 
 int main() {
-  printf("[Target] PID: %d\n", getpid());
-
-  while (1) {
-    trigger_breakpoint();
-    getchar();
-    // sleep(3);
-  }
-  return 0;
+    pthread_t t1, t2, t3;
+    pthread_create(&t1, NULL, worker_A, NULL);
+    pthread_create(&t2, NULL, worker_B, NULL);
+    pthread_create(&t3, NULL, worker_C, NULL);
+    pthread_exit(NULL);
 }
